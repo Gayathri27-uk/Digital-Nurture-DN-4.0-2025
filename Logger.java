b7edf575af7e8111b7d96b7d17b22def7f93cfbf
Bhuvana Gayathri <bhuvanagayathri2005@gmail.com>
2025-06-19 20:27:01 +0530

    Added solution for Singleton Pattern

--- Logger.java
+++ Logger.java
@@ -1,8 +1,21 @@
-import java.util.*;
-class Logger
+public class Logger
 {
-    public static void main(String[] args)
+    public static Logger singleInstance=null;
+    private Logger()
     {
-        System.out.println("Hello world");
+        System.out.println("Updated");
+    }
+    public static Logger getInstance()
+    {
+        if(singleInstance==null)
+        {
+            singleInstance=new Logger();
+        }
+        return singleInstance;
+    }
+    public void log(String name)
+    {
+        System.out.println("Name: "+name);
+
     }
 }
